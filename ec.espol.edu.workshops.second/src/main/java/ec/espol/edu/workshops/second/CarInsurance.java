cf2e22fdc62cdcf9fdeefb7c4922a1ee02d4a926
diego <diego@192.168.200.7>
2020-10-23 10:18:10 -0500

    Creation of class CarInsurance

---
+++ ec.espol.edu.workshops.second/src/main/java/ec/espol/edu/workshops/second/CarInsurance.java
@@ -0,0 +1,12 @@
+package ec.espol.edu.workshops.second;
+
+public class CarInsurance {
+	
+	private int age;
+	private String sex;
+	private String marrial_Status;
+	final int premium=500;
+	
+	
+	
+}
