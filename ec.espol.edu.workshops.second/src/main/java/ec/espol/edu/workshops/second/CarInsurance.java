07be7a66c51cee855f37e9b8828c76a2f7ca1f42
dianmoli98 <dianmoli@espol.edu.ec>
2020-10-23 11:19:01 -0500

    Create getter and setter methods

--- ec.espol.edu.workshops.second/src/main/java/ec/espol/edu/workshops/second/CarInsurance.java
+++ ec.espol.edu.workshops.second/src/main/java/ec/espol/edu/workshops/second/CarInsurance.java
@@ -5,7 +5,50 @@ public class CarInsurance {
 	private int age;
 	private String sex;
 	private String marrial_Status;
+	private int license;
 	final int premium=500;
+	
+	
+	public String getSex()
+    {
+            return sex;
+    }
+	
+	public String getMarrial_Status()
+    {
+            return marrial_Status;
+    }
+	
+	public int getAge()
+    {
+            return age;
+    }
+	
+	public int getLicense()
+    {
+            return license;
+    }
+	
+	public void setSex(String sex)
+    {
+      this.sex=sex;
+    }
+	
+	public void setMarrial_Status(String marrial_Status)
+    {
+      this.marrial_Status=marrial_Status;
+    }
+	
+	public void setAge(int age)
+    {
+      this.age = age;
+    }
+	
+	public void setLicense(int license)
+    {
+      this.license = license;
+    }
+	
 	
 	
 	
