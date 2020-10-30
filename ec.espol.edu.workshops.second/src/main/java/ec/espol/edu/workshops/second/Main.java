45ba6664d5b9e7cdb15a8f8191fc7d252520cd36
thaily samaniego <44647544+thsam@users.noreply.github.com>
2020-10-30 13:10:33 -0500

    .

---
+++ ec.espol.edu.workshops.second/src/main/java/ec/espol/edu/workshops/second/Main.java
@@ -0,0 +1,12 @@
+package ec.espol.edu.workshops.second;
+public class Main {
+
+	public static void main(String[] args) {
+
+		CarInsurance user1 = new CarInsurance("M",22,"married");
+		System.out.println(user1.calculo());
+	}
+		
+	
+
+}
